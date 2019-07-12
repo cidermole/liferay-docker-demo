526f0ea4b017089d37af1948c9b29b4906ca2007
MADL David <david.madl@qualysoft.com>
2019-07-12 15:15:13 +0200

    wait for mysql in startup.sh, use liferay version compatible with mysql
    Use Liferay 7.2.0-rc2 which is compatible with MySQL 8.0.16
    
    With 7.1.2-ga3 there were collisions with SQL keywords for table Company.
    Fields 'key' and 'system' have been renamed to 'key_' and 'system_' in Liferay 7.2.x

--- conf/liferay/mount/scripts/startup.sh
+++ conf/liferay/mount/scripts/startup.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-echo ""
-echo "*********************************************"
-echo " Your special startup script has been called"
-echo "*********************************************"
-echo ""
+TIMEOUT=60
+
+function wait_for {
+  echo "Waiting up to $TIMEOUT sec for service '$1' to be reachable at port $2 ..."
+
+  for i in `seq $TIMEOUT` ; do
+    nc -z "$1" "$2" > /dev/null 2>&1
+    
+    result=$?
+    if [ $result -eq 0 ] ; then
+      echo "Reached service '$1' at port $2"
+      exit 0
+    fi
+    sleep 1
+  done
+  echo "Operation timed out" >&2
+  exit 1
+}
+
+# parse JDBC string - split by '/', e.g. "jdbc:mysql://db/lportal?..." -> "db"
+urlParts=(${LIFERAY_JDBC_PERIOD_DEFAULT_PERIOD_URL//\// })
+database=${urlParts[1]}
+
+wait_for "$database" 3306
